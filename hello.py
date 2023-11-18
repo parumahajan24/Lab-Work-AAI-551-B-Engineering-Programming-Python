5814f4bd97230bd2ab3ebdafea24dd0a101ac700
Parul Mahajan <109793393+parumahajan24@users.noreply.github.com>
2023-11-18 15:34:57 -0500

    Delete hello.py

--- hello.py
+++
@@ -1,9 +0,0 @@
-# Author: Parul Mahajan (CWID: 20010763)
-# Date: 09/08/2023
-# Description: This is a very simple example of how to create a .py program in Pycharm IDE.
-# HELLO.PY file
-
-
-# Output texts to the screen :
-print('Hello, World!')
-print('** Welcome to Lab 01 for AAI 551-B course. This is a very simple example of how to create a .py program in Pycharm IDE **')
